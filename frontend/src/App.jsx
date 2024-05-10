a00d56b6c8dd0d995844b15427436cc5d82417ca
ravijainwk12 <ravijainwk12@gmail.com>
2024-05-10 17:38:23 +0530

    axios update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,8 @@ function App() {
 
   axios.defaults.withCredentials = true;
   useEffect(() => {
-    axios.get('http://localhost:3001/auth/verify')
+    // axios.get('http://localhost:3001/auth/verify')
+    axios.get('https://bookstore-frontend-blush.vercel.app/auth/verify')
     .then(res => {
       if(res.data.login) {
         setRole(res.data.role)
