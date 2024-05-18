3219fffe4303cfae717ec5ddc73e5eb28e973b88
ravijainwk12 <ravijainwk12@gmail.com>
2024-05-18 19:47:27 +0530

    img css updt

--- backend/frontend/src/components/Home.jsx
+++ backend/frontend/src/components/Home.jsx
@@ -13,8 +13,8 @@ const Home = () => {
             you will definitely find what you are looking for.
             </p>
         </div>
-        <div >
-           <img src='images\home_page.png' heigth="50%" width="auto"  />
+        <div className='hero-image' >
+           <img src='images\home_page.png'  />
         </div>
     </div>
   )
